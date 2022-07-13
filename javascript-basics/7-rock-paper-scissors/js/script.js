eeba102faed4a813fe3836a3e7bffe150b2fca19
Matthew Lewington <1703645@student.uwtsd.ac.uk>
2022-07-13 13:26:03 +0100

    Added playRound function

--- javascript-basics/7-rock-paper-scissors/js/script.js
+++ javascript-basics/7-rock-paper-scissors/js/script.js
@@ -12,3 +12,42 @@ function getPlayerInput ()
     if (choices.includes (selection))
         return selection;
 }
+
+function playRound (computerSelection, playerSelection)
+{
+    var result;
+
+    if (computerSelection == playerSelection)
+        result = "Draw";
+
+    else if (computerSelection == "rock")
+    {
+        if (playerSelection == "paper")
+            result = "Win";
+        else if (playerSelection == "scissors")
+            result = "Lose";
+    }
+
+    else if (computerSelection == "paper")
+    {
+        if (playerSelection == "rock")
+            result = "Lose";
+        else if (playerSelection == "scissors")
+            result = "Win";
+    }
+
+    else if (computerSelection == "scissors")
+    {
+        if (playerSelection == "rock")
+            result = "Win";
+        else if (playerSelection == "paper")
+            result = "Lose";
+    }
+
+    return result;
+}
+
+const computerSelection = computerPlay ();
+const playerSelection = getPlayerInput ();
+
+console.log (playRound (computerSelection, playerSelection));
